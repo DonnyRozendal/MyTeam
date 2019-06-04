1dc0442e8a3c903e034f36ace0e2552f13957cd5
Donny Rozendal <donnyrozendal@hotmail.com>
2019-06-05 01:12:54 +0200

    CHANGE - File location

--- app/src/main/java/nl/hva/myteam/features/presentation/pokedex/PokedexActivity.kt
+++ app/src/main/java/nl/hva/myteam/features/presentation/pokedex/PokedexActivity.kt
@@ -2,12 +2,18 @@ package nl.hva.myteam.features.presentation.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_pokedex.*
+import kotlinx.android.synthetic.main.view_pokedex_item.*
 import nl.hva.myteam.R
 import nl.hva.myteam.core.extension.observe
 import nl.hva.myteam.core.navigation.Navigator
 import nl.hva.myteam.features.data.models.Pokemon
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.File
 
 class PokedexActivity : AppCompatActivity() {
 
@@ -28,12 +34,15 @@ class PokedexActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
         viewModel.getAllPokemon()
-        assets
     }
 
     private fun handlePokedex(pokedex: List<Pokemon>) {
-
+        adapter.submitList(pokedex)
     }
 
 }
--- app/src/main/java/nl/hva/myteam/features/presentation/pokedex/PokedexAdapter.kt
+++ app/src/main/java/nl/hva/myteam/features/presentation/pokedex/PokedexAdapter.kt
@@ -1,11 +1,14 @@
 package nl.hva.myteam.features.presentation.pokedex
 
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.view_pokedex_item.view.*
 import nl.hva.myteam.R
 import nl.hva.myteam.features.data.models.Pokemon
 
@@ -31,8 +34,20 @@ class PokedexAdapter(private val clickListener: (Pokemon) -> Unit) :
     class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(pokemon: Pokemon, clickListener: (Pokemon) -> Unit) = with(view) {
-            val images = context.assets.list("sprites/pokemon")
-            val ips = context.assets.open("sprites/pokemon/" + images?.get(adapterPosition))
+            val entry = Regex("""(?<!v)\d+""").find(pokemon.url)?.value
+            val ips = resources.openRawResource(
+                resources.getIdentifier(
+                    "default_$entry",
+                    "raw",
+                    context.packageName
+                )
+            )
+            val bitmap = BitmapFactory.decodeStream(ips)
+            imageViewSprite.setImageBitmap(bitmap)
+            ips.close()
+
+            textViewName.text = pokemon.name.capitalize()
+            textViewEntry.text = context.getString(R.string.entry, entry)
 
             setOnClickListener { clickListener(pokemon) }
         }
